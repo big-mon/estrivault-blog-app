1b891e7d4056adce6322878e1a04d8193703cb81
BIGMON <mrmon93+github@gmail.com>
2025-04-19 18:51:38 +0900

    update: add markdown collection config to astro.config.mjs

--- apps/astro-blog/astro.config.mjs
+++ apps/astro-blog/astro.config.mjs
@@ -2,10 +2,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineCollection, z } from 'astro:content';
+
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      publish: z.boolean().default(true),
+      date: z.date(),
+    }),
+  }),
+};
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://{preview_domain}',
-	output: 'static',
-	integrations: [mdx(), sitemap()],
+  site: 'https://{preview_domain}',
+  output: 'static',
+  integrations: [mdx(), sitemap()],
 });
