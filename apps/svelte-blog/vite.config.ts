88333d4cf903b9f5a6f2422e9cf387f3e1db80c4
MON <mrmon93+github@gmail.com>
2025-05-28 03:27:29 +0900

    add: コンテンツ処理パッケージとSvelteKitのVite設定を追加

--- apps/svelte-blog/vite.config.ts
+++ apps/svelte-blog/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
   envDir: process.cwd(),
-  ssr: {
-    noExternal: ['fast-glob'],
+  optimizeDeps: {
+    exclude: ['fast-glob'],
   },
 });
