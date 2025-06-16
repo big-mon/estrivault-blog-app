db7cd84ca3280d6ebf307f142c18b64f42b099be
MON <mrmon93+github@gmail.com>
2025-06-16 15:13:03 +0000

    refactor: 非推奨を削除

--- packages/content-processor/src/index.ts
+++ packages/content-processor/src/index.ts
@@ -9,9 +9,3 @@ export { createPipeline } from './pipeline';
 
 // ユーティリティ
 export { normalizeForTagFilter, normalizeForSlug } from './utils/normalize';
-
-// 非推奨：ファイル読み込み機能は個別アプリケーションで実装してください
-// @deprecated ファイル読み込み機能はsvelte-blog側に移行されました
-export { loadFile } from './loaders/file-loader';
-// @deprecated ファイル読み込み機能はsvelte-blog側に移行されました  
-export { walkMarkdownFiles, walkMarkdownFilesWithPath } from './utils/file-walker';
