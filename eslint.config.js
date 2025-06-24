bcbdaadf6845859d58e665313450b4aa52271ca5
big-mon <mrmon93+github@gmail.com>
2025-06-24 13:03:50 +0000

    feat: ESLint設定を追加し、lintスクリプトをpackage.jsonに追加

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import svelteConfig from './apps/svelte-blog/svelte.config.js';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
@@ -32,5 +32,5 @@ export default ts.config(
         svelteConfig,
       },
     },
-  }
+  },
 );
