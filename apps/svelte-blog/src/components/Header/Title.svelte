5dc5010650d110e7c5632a3e9e5ed31cc8110be2
MON <mrmon93+github@gmail.com>
2026-03-06 23:11:43 +0900

    chore: remove legacy svelte-blog app and stale agent docs

--- apps/svelte-blog/src/components/Header/Title.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { SITE_TITLE } from '../../constants/index';
-</script>
-
-<span class="text-xl font-bold text-gray-900">
-  <a href="/" class="transition-colors hover:text-gray-700">
-    {SITE_TITLE}
-  </a>
-</span>
